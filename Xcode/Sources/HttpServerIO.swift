d97a0e1b1a5df9b63a0e0a070f1d68983cee10a2
Iskandar Safarov <sapharow@gmail.com>
2025-08-31 18:15:30 +1000

    Better race condition fix

--- Xcode/Sources/HttpServerIO.swift
+++ Xcode/Sources/HttpServerIO.swift
@@ -12,6 +12,49 @@ public protocol HttpServerIODelegate: AnyObject {
     func socketConnectionReceived(_ socket: Socket)
 }
 
+@propertyWrapper
+final class Locked<Value> {
+    private var value: Value
+
+    private var lock = os_unfair_lock_s()
+    private var mtx = pthread_mutex_t()
+
+    init(wrappedValue: Value) {
+        self.value = wrappedValue
+        if #available(iOS 10.0, *) {
+            lock = os_unfair_lock_s()
+        } else {
+            mtx = pthread_mutex_t()
+            var attr = pthread_mutexattr_t()
+            pthread_mutexattr_init(&attr)
+            pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_NORMAL)
+            pthread_mutex_init(&mtx, &attr)
+            pthread_mutexattr_destroy(&attr)
+        }
+    }
+
+    var wrappedValue: Value {
+        get { with { $0 } }
+        set { with { $0 = newValue } }
+    }
+
+    // Atomic read-modify-write
+    var projectedValue: Locked<Value> { self }
+
+    @inline(__always)
+    private func with<R>(_ body: (inout Value) throws -> R) rethrows -> R {
+        if #available(iOS 10.0, *) {
+            os_unfair_lock_lock(&lock)
+            defer { os_unfair_lock_unlock(&lock) }
+            return try body(&value)
+        } else {
+            pthread_mutex_lock(&mtx)
+            defer { pthread_mutex_unlock(&mtx) }
+            return try body(&value)
+        }
+    }
+}
+
 open class HttpServerIO {
 
     public weak var delegate: HttpServerIODelegate?
@@ -26,21 +69,7 @@ open class HttpServerIO {
         case stopped
     }
 
-    private var stateValue: Int32 = HttpServerIOState.stopped.rawValue
-
-    public private(set) var state: HttpServerIOState {
-        get {
-            let value = OSAtomicAnd32(0xffffffff, &stateValue)
-            return HttpServerIOState(rawValue: value)!
-        }
-        set(state) {
-            #if !os(Linux)
-            OSAtomicCompareAndSwapInt(self.state.rawValue, state.rawValue, &stateValue)
-            #else
-            self.stateValue = state.rawValue
-            #endif
-        }
-    }
+    @Locked<HttpServerIOState> public var state = .stopped
 
     public var operating: Bool { return self.state == .running }
 
